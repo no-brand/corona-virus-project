284764eb42f9a019cd203af0fa2fd7dd50128434
no-brand <do.dream.david@gmail.com>
2020-02-01 23:01:10 +0900

    feat(data): get pandas from external data
    usage
    > from model import *
    > KoreanTrail().dataframe
    > WorldCase().dataframe

---
+++ model/korea.py
@@ -0,0 +1,28 @@
+from dao.sheets import *
+import pandas as pd
+
+
+class KoreanTrail:
+    def __init__(self):
+        self.spread_sheet_id = '1xJ-VxfKWByj4HyGQjNWyvFgEopbIQSwhPBvzWUFHnko'
+        self.sheet_range = 'korea'
+        self.data = SheetService().get(spread_sheet_id=self.spread_sheet_id,
+                                       doc_range=self.sheet_range)
+        assert self.data is not None
+        assert isinstance(self.data, list)
+        self.columns = self.data[0]
+        self.__data = self.data[1:]
+        self.__dataframe = pd.DataFrame(self.__data,
+                                        index=None,
+                                        columns=self.columns)
+        self.__type_cast()
+
+    def __type_cast(self):
+        self.__dataframe = self.__dataframe.astype({
+            'id': 'int32', 'age': 'int32',
+            'latitude': 'float64', 'longitude': 'float64'
+        })
+
+    @property
+    def dataframe(self):
+        return self.__dataframe
---
+++ model/world.py
@@ -0,0 +1,27 @@
+from dao.sheets import *
+import pandas as pd
+
+
+class WorldCase:
+    def __init__(self):
+        self.spread_sheet_id = '1yZv9w9zRKwrGTaR-YzmAqMefw4wMlaXocejdxZaTs6w'
+        self.sheet_range = 'Jan31_7pm'
+        self.data = SheetService().get(spread_sheet_id=self.spread_sheet_id,
+                                       doc_range=self.sheet_range)
+        assert self.data is not None
+        assert isinstance(self.data, list)
+        self.columns = self.data[0]
+        self.__data = self.data[1:]
+        self.__dataframe = pd.DataFrame(self.__data,
+                                        index=None,
+                                        columns=self.columns)
+        self.__type_cast()
+
+    def __type_cast(self):
+        self.__dataframe['Confirmed'] = pd.to_numeric(self.__dataframe['Confirmed']).fillna(0).astype('int64')
+        self.__dataframe['Deaths'] = pd.to_numeric(self.__dataframe['Deaths']).fillna(0).astype('int64')
+        self.__dataframe['Recovered'] = pd.to_numeric(self.__dataframe['Recovered']).fillna(0).astype('int64')
+
+    @property
+    def dataframe(self):
+        return self.__dataframe
